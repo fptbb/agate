e51d5feb8a871a0705f97bc732d4c4c3fa3482c5
Lucas "fptbb" Oliveira <lucas@fptbb.com>
2025-10-05 21:35:02 -0300

    add ublue branding

---
+++ files/scripts/branding.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Your code goes here.
+IMAGE_INFO="/usr/share/ublue-os/image-info.json"
+IMAGE_REF="ostree-image-signed:docker://ghcr.io/fptbb/fp-os-next"
+
+# image-info File
+sed -i 's/"image-name": [^,]*/"image-name": "'"fp-os"'"/' $IMAGE_INFO
+sed -i 's/"image-vendor": [^,]*/"image-vendor": "'"fptbb"'"/' $IMAGE_INFO
+sed -i 's|"image-ref": [^,]*|"image-ref": "'"$IMAGE_REF"'"|' $IMAGE_INFO
