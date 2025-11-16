f3a93bbf5e144a1b7328a8712892ac968329ff2a
Lucas "fptbb" Oliveira <lucas@fptbb.com>
2025-11-16 09:37:43 -0300

    Add Whatpulse Support

--- files/scripts/custom-rpm.sh
+++ files/scripts/custom-rpm.sh
@@ -11,4 +11,4 @@ log() {
 log "Install Custom RPM file - Whatpulse Pcap Service"
 
 curl -s https://api.github.com/repos/whatpulse/linux-external-pcap-service/releases/latest | jq -r '.assets[] | select(.name | endswith(".x86_64.rpm")) | .browser_download_url' | xargs curl -L -O
-sudo rpm -i whatpulse-pcap-service-*.x86_64.rpm
+rpm -i whatpulse-pcap-service-*.x86_64.rpm
