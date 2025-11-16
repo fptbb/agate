2c18b12cbecf20fcf8f5b43f91fc5f29466aeafe
Lucas "fptbb" Oliveira <lucas@fptbb.com>
2025-11-16 14:53:54 -0300

    Update whatpulse.sh

--- files/scripts/whatpulse.sh
+++ files/scripts/whatpulse.sh
@@ -8,10 +8,10 @@ log() {
   echo "=== $* ==="
 }
 
-log "Build whatpulse service."
+# log "Build whatpulse service."
 
-git clone https://github.com/whatpulse/linux-external-pcap-service.git
-cd ./linux-external-pcap-service
-make
-install -m 755 ./whatpulse-pcap-service /usr/bin/whatpulse-pcap-service
-install -m 644 ./whatpulse-pcap-service.service /lib/systemd/system/whatpulse-pcap-service.service
+# git clone https://github.com/whatpulse/linux-external-pcap-service.git
+# cd ./linux-external-pcap-service
+# make
+# install -m 755 ./whatpulse-pcap-service /usr/bin/whatpulse-pcap-service
+# install -m 644 ./whatpulse-pcap-service.service /lib/systemd/system/whatpulse-pcap-service.service
