11f581cc60c3f34008b355607ea6d33cb1597d1f
Lucas "fptbb" Oliveira <lucas@fptbb.com>
2026-03-24 09:20:49 -0300

    feat: manage fp-appimage-updater as a COPR package and disable its direct script update.

--- files/scripts/appimage-updater.sh
+++ files/scripts/appimage-updater.sh
@@ -8,10 +8,10 @@ log() {
   echo "=== $* ==="
 }
 
-log "Updating AppImage Updater..."
+# log "Updating AppImage Updater..."
 # Use alternative url with jsdelivr to avoid github rate limits
-curl -sL https://cdn.jsdelivr.net/gh/fptbb/fp-appimage-updater@latest/install.sh | bash -s -- --system --no-systemd
-log "AppImage Updater updated successfully."
+# curl -sL https://cdn.jsdelivr.net/gh/fptbb/fp-appimage-updater@latest/install.sh | bash -s -- --system --no-systemd
+# log "AppImage Updater updated successfully."
 
 # appends a new path before the closing bracket of the existing array
 sed -i 's|^\(paths = \[.*\)\]|\1, "/etc/ublue-os/fp-topgrade.toml"]|' /usr/share/ublue-os/topgrade.toml
