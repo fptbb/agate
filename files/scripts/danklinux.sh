8d8726f5d6b919ce07bc0c36b87a2fe87e3abded
Lucas "fptbb" Oliveira <lucas@fptbb.com>
2026-04-01 12:29:45 -0300

    feat: try kwallet in niri

--- files/scripts/danklinux.sh
+++ files/scripts/danklinux.sh
@@ -8,5 +8,8 @@ mkdir -p /usr/lib/systemd/user/niri.service.wants
 ln -sf /usr/lib/systemd/user/dms.service /usr/lib/systemd/user/niri.service.wants/dms.service
 ln -sf /usr/lib/systemd/user/dsearch.service /usr/lib/systemd/user/niri.service.wants/dsearch.service
 
-ln -sf /usr/lib/systemd/user/gnome-keyring-daemon.service /usr/lib/systemd/user/niri.service.wants/gnome-keyring-daemon.service
-ln -sf /usr/lib/systemd/user/gnome-keyring-daemon.socket /usr/lib/systemd/user/niri.service.wants/gnome-keyring-daemon.socket
+ln -sf /usr/lib/systemd/user/plasma-kwallet-pam.service /usr/lib/systemd/user/niri.service.wants/plasma-kwallet-pam.service
+
+# Use gnome keyring
+# ln -sf /usr/lib/systemd/user/gnome-keyring-daemon.service /usr/lib/systemd/user/niri.service.wants/gnome-keyring-daemon.service
+# ln -sf /usr/lib/systemd/user/gnome-keyring-daemon.socket /usr/lib/systemd/user/niri.service.wants/gnome-keyring-daemon.socket
