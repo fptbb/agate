ac6a91e8fa7ee6f500f31c99d3657621366cbb88
Lucas "fptbb" Oliveira <lucas@fptbb.com>
2026-04-01 09:11:32 -0300

    feat: add gnome-keyring.service to niri systemd user session startup

--- files/scripts/danklinux.sh
+++ files/scripts/danklinux.sh
@@ -7,3 +7,6 @@ echo "Configuring DankLinux (DMS) for Niri Wayland Session..."
 mkdir -p /usr/lib/systemd/user/niri.service.wants
 ln -sf /usr/lib/systemd/user/dms.service /usr/lib/systemd/user/niri.service.wants/dms.service
 ln -sf /usr/lib/systemd/user/dsearch.service /usr/lib/systemd/user/niri.service.wants/dsearch.service
+
+ln -sf /usr/lib/systemd/user/gnome-keyring-daemon.service /usr/lib/systemd/user/niri.service.wants/gnome-keyring-daemon.service
+ln -sf /usr/lib/systemd/user/gnome-keyring-daemon.socket /usr/lib/systemd/user/niri.service.wants/gnome-keyring-daemon.socket
