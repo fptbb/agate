79bad1824796a49d705fe44d42a1a75fba14a3aa
Lucas "fptbb" Oliveira <lucas@fptbb.com>
2026-04-01 16:30:18 -0300

    fix: try fix qt6 theming

--- files/scripts/danklinux.sh
+++ files/scripts/danklinux.sh
@@ -13,3 +13,6 @@ ln -sf /usr/lib/systemd/user/plasma-kwallet-pam.service /usr/lib/systemd/user/ni
 # Use gnome keyring
 # ln -sf /usr/lib/systemd/user/gnome-keyring-daemon.service /usr/lib/systemd/user/niri.service.wants/gnome-keyring-daemon.service
 # ln -sf /usr/lib/systemd/user/gnome-keyring-daemon.socket /usr/lib/systemd/user/niri.service.wants/gnome-keyring-daemon.socket
+
+mkdir -p /etc/environment.d
+echo 'QT_QPA_PLATFORMTHEME=qt6ct' > /etc/environment.d/99-qt_theme.conf
