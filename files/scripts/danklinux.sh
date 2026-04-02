14bb423d4f8522e095625293be143f151317d0f8
Lucas "fptbb" Oliveira <lucas@fptbb.com>
2026-04-01 22:11:56 -0300

    feat: configure xdg-desktop-portal defaults and install necessary portal backends for niri

--- files/scripts/danklinux.sh
+++ files/scripts/danklinux.sh
@@ -14,5 +14,16 @@ ln -sf /usr/lib/systemd/user/plasma-kwallet-pam.service /usr/lib/systemd/user/ni
 # ln -sf /usr/lib/systemd/user/gnome-keyring-daemon.service /usr/lib/systemd/user/niri.service.wants/gnome-keyring-daemon.service
 # ln -sf /usr/lib/systemd/user/gnome-keyring-daemon.socket /usr/lib/systemd/user/niri.service.wants/gnome-keyring-daemon.socket
 
-mkdir -p /etc/environment.d
-echo 'QT_QPA_PLATFORMTHEME=qt6ct' > /etc/environment.d/99-qt_theme.conf
+PORTAL_CONF="/usr/share/xdg-desktop-portal/niri-portals.conf"
+
+if [ ! -f "$PORTAL_CONF" ]; then
+    echo "Configuration file not found at $PORTAL_CONF"
+    exit 1
+fi
+
+sed -i 's/^default=.*/default=kde;gtk;wlr/' "$PORTAL_CONF"
+sed -i 's/^org.freedesktop.impl.portal.Access=.*/org.freedesktop.impl.portal.Access=kde;gtk;/' "$PORTAL_CONF"
+sed -i 's/^org.freedesktop.impl.portal.Notification=.*/org.freedesktop.impl.portal.Notification=kde;gtk;/' "$PORTAL_CONF"
+sed -i 's/^org.freedesktop.impl.portal.Secret=.*/org.freedesktop.impl.portal.Secret=kde;/' "$PORTAL_CONF"
+
+echo "Portal configuration updated successfully."
