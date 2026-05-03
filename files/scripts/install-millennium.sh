a4f9f2fb486f6aab967c3b378fdf19e301890a16
Lucas "fptbb" Oliveira <lucas@fptbb.com>
2026-05-03 17:57:48 -0300

    chore:  further fix tor

--- files/scripts/install-millennium.sh
+++ files/scripts/install-millennium.sh
@@ -4,15 +4,16 @@ set -ouex pipefail
 RELEASE_TAG=$(curl -s "https://api.github.com/repos/SteamClientHomebrew/Millennium/releases/latest" | jq -r '.tag_name')
 DOWNLOAD_URL="https://github.com/SteamClientHomebrew/Millennium/releases/download/${RELEASE_TAG}/millennium-${RELEASE_TAG}-linux-x86_64.tar.gz"
 
-mkdir -p /tmp/millennium
-curl -sSL "$DOWNLOAD_URL" -o /tmp/millennium/millennium.tar.gz
+TEMP_DIR=$(mktemp -d)
 
-tar -xzf /tmp/millennium/millennium.tar.gz -C /tmp/millennium
+curl -sSL "$DOWNLOAD_URL" -o "$TEMP_DIR/millennium.tar.gz"
 
-cp -rP /tmp/millennium/usr/* /usr/
+tar -xzf "$TEMP_DIR/millennium.tar.gz" -C "$TEMP_DIR"
+
+cp -rP "$TEMP_DIR/usr/"* /usr/
 
 mkdir -p /usr/share/millennium-opt
-cp -rP /tmp/millennium/opt/* /usr/share/millennium-opt/
+cp -rP "$TEMP_DIR/opt/"* /usr/share/millennium-opt/
 
 # extracts the exact python directory name safely using a bash glob
 for dir in /usr/share/millennium-opt/python-i686*; do
@@ -26,4 +27,4 @@ chmod +x "/usr/share/millennium-opt/${PYTHON_DIR}/bin/"*
 # generates tmpfiles.d config to dynamically map opt into var/opt on boot
 echo "C /var/opt/${PYTHON_DIR} - - - - /usr/share/millennium-opt/${PYTHON_DIR}" > /usr/lib/tmpfiles.d/millennium.conf
 
-rm -rf /tmp/millennium
+rm -rf "$TEMP_DIR"
